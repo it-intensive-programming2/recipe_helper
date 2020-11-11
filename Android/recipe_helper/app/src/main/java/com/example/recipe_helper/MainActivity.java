21fe634a8571f033539ee3d1788f2641b130de65
rhcsky <mangusn1@gmail.com>
2020-11-11 18:11:06 +0900

    First commit about android

---
+++ Android/recipe_helper/app/src/main/java/com/example/recipe_helper/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.recipe_helper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigation;
+    private Home home;
+    private MyPage my_page;
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.item_home:
+                    replaceFragment(home);
+                    return true;
+                case R.id.item_mypage:
+                    replaceFragment(my_page);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        home = new Home();
+        my_page = new MyPage();
+
+        navigation = findViewById(R.id.nav_bar);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.item_home);
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_main, fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        manager.popBackStackImmediate();
+        transaction.commit();
+    }
+
+    public void replaceFragmentFull(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+}
