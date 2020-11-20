8bfcbdcba1241b0d547eb86f9d98eb26b9666b1f
rhcsky <mangusn1@gmail.com>
2020-11-20 22:41:32 +0900

    update files

--- Android/recipe_helper/app/src/main/java/com/example/recipe_helper/Mypage/MyPage.java
+++ Android/recipe_helper/app/src/main/java/com/example/recipe_helper/Mypage/MyPage.java
@@ -15,6 +15,8 @@ import com.example.recipe_helper.R;
 public class MyPage extends Fragment {
     private int user_state = 0;
     private ImageView iv;
+    private ImageView iv2;
+    private ImageView iv3;
 
     @Nullable
     @Override
@@ -25,12 +27,38 @@ public class MyPage extends Fragment {
 
 
         iv = (ImageView) view.findViewById(R.id.HBP1);
+        iv2 = (ImageView) view.findViewById(R.id.HBP2);
+        iv3 = (ImageView) view.findViewById(R.id.HBP3);
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 user_state++;
-                if (user_state > 2) user_state = 0;
+                if (user_state > 2) {
+                    user_state = 0;
+                }
                 iv.setImageResource(ImageId[user_state]);
+            }
+        });
+
+        iv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_state++;
+                if (user_state > 2) {
+                    user_state = 0;
+                }
+                iv2.setImageResource(ImageId[user_state]);
+            }
+        });
+
+        iv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_state++;
+                if (user_state > 2) {
+                    user_state = 0;
+                }
+                iv3.setImageResource(ImageId[user_state]);
             }
         });
 
