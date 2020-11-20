e649d01a7979facc8ef97ad89304b8330f310f61
horseman-97 <ndhyun3847@ajou.ac.kr>
2020-11-20 14:30:29 +0900

    merge commit

--- Android/recipe_helper/app/src/main/java/com/example/recipe_helper/Mypage/MyPage.java
+++ Android/recipe_helper/app/src/main/java/com/example/recipe_helper/Mypage/MyPage.java
@@ -16,6 +16,7 @@ import com.example.recipe_helper.R;
 
 public class MyPage extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
