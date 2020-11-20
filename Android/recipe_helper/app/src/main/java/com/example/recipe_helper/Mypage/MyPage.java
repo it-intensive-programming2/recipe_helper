9948bc871beccb380d551ae1914824e5620b898b
horseman-97 <ndhyun3847@ajou.ac.kr>
2020-11-20 19:28:56 +0900

    MyPage.java

--- Android/recipe_helper/app/src/main/java/com/example/recipe_helper/Mypage/MyPage.java
+++ Android/recipe_helper/app/src/main/java/com/example/recipe_helper/Mypage/MyPage.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,15 +13,27 @@ import androidx.fragment.app.Fragment;
 import com.example.recipe_helper.R;
 
 public class MyPage extends Fragment {
-
+    private int user_state = 0;
+    private ImageView iv;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mypage, container, false);
         view.setClickable(true);
+        final int[] ImageId = {R.drawable.ic_sad, R.drawable.ic_neutral, R.drawable.ic_smiling};
+
+
+        iv = (ImageView) view.findViewById(R.id.imageView);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv.setImageResource(ImageId[user_state]);
+            }
+        });
 
         //Todo Something
         return view;
     }
+
 }
